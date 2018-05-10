079c2ca0cfab5337af4458c58f2d35457db61e2c
Filip <fhagno@yahoo.com>
2018-05-10 14:17:59 +0200

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDZOKEkyreaxZgUv246UuAZl7zxLqCI7wA',
+      authDomain: 'ng-recipe-book-9f137.firebaseapp.com',
+      databaseURL: 'https://ng-recipe-book-9f137.firebaseio.com',
+      projectId: 'ng-recipe-book-9f137',
+      storageBucket: 'ng-recipe-book-9f137.appspot.com',
+      messagingSenderId: '389221242965'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+}
